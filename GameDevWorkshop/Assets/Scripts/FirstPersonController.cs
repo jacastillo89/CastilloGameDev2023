f84c7f319d36cc2313a31b1a038b4a1b07e5c388
jacastillo89 <46500032+jacastillo89@users.noreply.github.com>
2023-09-12 10:49:19 -0400

    script for move look and jump
    script for move look and jump, ad values and call library

--- GameDevWorkshop/Assets/Scripts/FirstPersonController.cs
+++ GameDevWorkshop/Assets/Scripts/FirstPersonController.cs
@@ -1,9 +1,37 @@
+/***Jorge Castillo 2023.09.12
+Script for basic first person and character controls
+
+
+
+***/
+
+//Libraries that we're using
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FirstPersonController : MonoBehaviour
+
 {
+    //Player Variables
+    public float speed;
+    public float gravity;
+    public float jumpForce;
+    
+    //Movement and Looking 
+    private CharacterController characterController;
+    private Vector2 moveInput;
+    private Vector3 playerVelocity;
+    private bool grounded;
+    private Vector2 mouseMovement;
+
+    //Camera vairables
+    public Camera cameraLive;
+    public float sensitivity;
+    private float camXRotation;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +43,17 @@ public class FirstPersonController : MonoBehaviour
     {
         
     }
+    public void onMove(InputAction.CallbackContext contex)
+    {
+        
+    }
+    public void onLook(InputAction.CallbackContext contex)
+    {
+
+    }
+    public void onJump(InputAction.CallbackContext contex)
+    {
+
+    }
 }
+
